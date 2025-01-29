26841c0b3c53432e05cd22af1c6a9a84b59940ca
ap-developer-18 <puniaaman888@gmail.com>
2025-01-29 14:56:09 +0530

    work on page setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
     content: ["./index.html"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#21222B",
+                secondary: "#6842D9",
+                black: "#000000",
+                blue: "#3D2388",
+                gray: {
+                    200: "#F3F4F5",
+                    300: "#CBCED4",
+                },
+            },
+            lineHeight: {
+                100: "100%",
+            },
+            boxShadow: {
+                custom: "0px 4px 4px 0px #00000040",
+            },
+            backgroundImage: {
+                "blue-gradient": "linear-gradient(92.88deg, #9977FF 11.2%, #6741D9 82%)",
+            },
+        },
     },
     plugins: [],
 };
